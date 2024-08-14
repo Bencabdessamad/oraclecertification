03c73557eecf588646b1aa58ac37cf01053dccd9
Bencabdessamad <ab.bencheraik@gmail.com>
2024-08-14 23:37:46 +0100

    third class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
 import Product.Product;
-
+import Product.ProductModel;
 import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        Product p1 = new Product();
+        /*Product p1 = new Product();
         p1.setPrice(1.99);
         BigDecimal price = p1.getPrice();
-        System.out.println(price);
+        System.out.println(price);*/
+
+        new ProductModel();
+
+        ProductModel p1 = new ProductModel();
+        ProductModel p2 = new ProductModel();
+        ProductModel p3 = p2;
+        p1.setName("Tea");
+        p2.setName("Cake");
+        System.out.println(p1.getName()+" in a cup");
+        System.out.println(p2.getName()+" on a plate");
+        System.out.println(p3.getName()+" to share");
     }
 }
